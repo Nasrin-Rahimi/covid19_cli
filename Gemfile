f9b12c0b5d727d192e6bce24dc6090d16168ac38
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-08-21 23:42:44 -0400

    edit environment

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in covid19_cli.gemspec
 gemspec
 
-gem "rake", "~> 12.0"
+# gem "rake", "~> 12.0"
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,16 @@
 require "bundler"
 # Require the gems from the Gemfile using bundler
 
+module Covid19
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'pry'
+
 require_relative "../lib/country.rb"
 require_relative "../lib/cli.rb"
 require_relative "../lib/api.rb"
