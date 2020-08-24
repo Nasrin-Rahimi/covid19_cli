162d38fa5ee2913b34695715bce4bba87ac82cd0
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-08-24 06:48:33 -0400

    delete unwanted lines

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'net/http'
 require 'json'
-require 'pry'
 
 class Covid19::API
 
@@ -15,19 +14,8 @@ class Covid19::API
   
   # we use the JSON library to parse the API response into nicely formatted JSON
   def self.get_countries
-    
     countries = JSON.parse(self.get_request)
     Covid19::Country.new_from_collection(countries["data"])
-   
-    # way 1
-   
-    # countries["data"].collect do |country|
-        
-    #     # Covid19::Country.new(country)
-    # end
-
-    # way 2
-    # Covid19::Country.new_from_collection(countries)
   end
   
 end
