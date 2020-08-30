744ef93fb35ead45e38e4ced4e0d0e58c309e2a8
Nasrin-Rahimi <77.nasrin@gmail.com>
2020-08-30 05:58:17 -0400

    remove unwanted attr from country class

--- lib/country.rb
+++ lib/country.rb
@@ -1,12 +1,11 @@
 class Covid19::Country
 
-    attr_accessor :name, :code, :population, :updated_at, :today_deaths, :today_confirmed, :latest_deaths, :latest_confirmed, :recovered, :critical,  :death_rate, :recovery_rate, :recovered_vs_death_ratio, :cases_per_million_population 
+    attr_accessor :name, :population, :updated_at, :today_deaths, :today_confirmed, :latest_deaths, :latest_confirmed, :recovered, :death_rate, :recovery_rate, :recovered_vs_death_ratio, :cases_per_million_population 
 
     @@all = []
 
     def initialize(data)
         @name = data["name"]
-        @code = data["code"]
         @population = data["population"]
         @updated_at = data["updated_at"]
         @today_deaths = data["today"]["deaths"]
@@ -14,7 +13,6 @@ class Covid19::Country
         @latest_deaths = data["latest_data"]["deaths"]
         @latest_confirmed = data["latest_data"]["confirmed"]
         @recovered = data["latest_data"]["recovered"]
-        @critical = data["latest_data"]["critical"]
         @death_rate = data["latest_data"]["calculated"]["death_rate"]
         @recovery_rate = data["latest_data"]["calculated"]["recovery_rate"]
         @recovered_vs_death_ratio = data["latest_data"]["calculated"]["recovered_vs_death_ratio"]
@@ -42,13 +40,11 @@ class Covid19::Country
 
     def self.find_by_name(name)
         all
-        binding.pry
         @@all.detect{|c| c.name == name.downcase}
 
     end
 
     # def initialize(attrs)
-    #     set_int_id
     #     attrs_from_hash(attrs)
     #     save
     #   end
